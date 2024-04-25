6d4f93b9b8c285e2843fd543135c9f2ea150bf86
DavidTomato <111129902+DavidTomato@users.noreply.github.com>
2024-04-25 16:03:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -96,11 +96,11 @@ Pair * firstMap(HashMap * map) {
     while(x < map->capacity){
         if(map->buckets[x] != NULL){
           map->current = x;
+          printf("No hay elementos en la hash map");
           return map->buckets[x];
         }
         x++;
     }
-    printf("No hay elementos en la hash map");
     return NULL;
 }
 
